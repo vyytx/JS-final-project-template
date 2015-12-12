b1a0cdaff8d2c27f103ebd00ab7f99fcde32bf92
vyytx <dobeender@gmail.com>
2015-12-12 13:01:43 +0800

    ghhg

---
+++ main.js
@@ -0,0 +1,18 @@
+// 創造 img HTML 元素，並放入變數中
+var bgImg = document.createElement("img");
+
+// 設定這個元素的要顯示的圖片
+bgImg.src = "images/map.png";
+
+// 找出網頁中的 canvas 元素
+var canvas = document.getElementById("game-canvas");
+
+// 取得 2D繪圖用的物件
+var ctx = canvas.getContext("2d");
+function draw(){
+    // 將背景圖片畫在 canvas 上的 (0,0) 位置
+ctx.drawImage(bgImg,0,0);
+}
+
+// 執行 draw 函式
+draw();
