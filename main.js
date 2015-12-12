bf3676c63d62c8c37ab8770d730f01bae3521f62
vyytx <dobeender@gmail.com>
2015-12-12 13:40:03 +0800

    1231435657687909
    wteryuiop[

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
+
 // 創造 img HTML 元素，並放入變數中
 var bgImg = document.createElement("img");
-
+var SlimeImg = document.createElement("img");
 // 設定這個元素的要顯示的圖片
 bgImg.src = "images/map.png";
-
+SlimeImg.src = "images/slime.png";
 // 找出網頁中的 canvas 元素
 var canvas = document.getElementById("game-canvas");
 
@@ -12,7 +13,8 @@ var ctx = canvas.getContext("2d");
 function draw(){
     // 將背景圖片畫在 canvas 上的 (0,0) 位置
 ctx.drawImage(bgImg,0,0);
+  ctx.drawImage(SlimeImg,0,0);
 }
-
 // 執行 draw 函式
 draw();
+setTimeout( draw, 1000);
