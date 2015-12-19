92aa8dc1e0dd13b5e2e6147adbbc1ea9d6e919ac
vyytx <dobeender@gmail.com>
2015-12-19 11:57:57 +0800

    ioihobhubvud
    fyyugk

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-
 // 創造 img HTML 元素，並放入變數中
 var bgImg = document.createElement("img");
 var SlimeImg = document.createElement("img");
-var slime ={
-  x : 128;
-  y : 480;
-};
+// var slime ={
+//   x : 128;
+//   y : 480;
+// };
 // 設定這個元素的要顯示的圖片
 bgImg.src = "images/map.png";
 SlimeImg.src = "images/slime.gif";
@@ -17,7 +16,7 @@ var ctx = canvas.getContext("2d");
 function draw(){
 // 將背景圖片畫在 canvas 上的 (0,0) 位置
 ctx.drawImage(bgImg,0,0);
-ctx.drawImage(SlimeImg,slime.x,slime.y);
+ctx.drawImage(SlimeImg,128,480);
 }
 // 執行 draw 函式
 // draw();
