c6cdb9b0175c27b81d305f63a15dde4ef4618462
vyytx <dobeender@gmail.com>
2015-12-26 10:24:25 +0800

    uihg
    tfttt

--- main.js
+++ main.js
@@ -1,22 +1,17 @@
 var IsBuilding = false;
 var cursor = {x:0, y:0};
-$(#game-canvas)
+
 $("#game-canvas").mousemove( function(event) {
 console.log("x:"+event.pageX+",y:"+event.pageY);
 cursor.x=event.pageX;
 cursor.y=event.pageY;
-  
-});
+  });
 
 // 創造 img HTML 元素，並放入變數中
 var bgImg = document.createElement("img");
 var SlimeImg = document.createElement("img");
 var SlimeTowerImg = document.createElement("img");
 var BadTowerImg = document.createElement("img");
-// var slime ={
-//   x : 128;
-//   y : 480;
-// };
 // 設定這個元素的要顯示的圖片
 bgImg.src = "images/map.png";
 SlimeImg.src = "images/slime.gif";
