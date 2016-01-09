62aa6e9b377131f3c5a4c88d409265c5d9149e4e
vyytx <dobeender@gmail.com>
2016-01-09 12:51:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var FPS = 720
+var FPS = 60
 var IsBuilding = false;
 var cursor = {x:0, y:0};
 var enemy = { 
@@ -7,11 +7,28 @@ var enemy = {
     speed:64,
     direction:{x:0, y:-1},
     pathDes : 0,
-    move:function (){
-        this.x += this.direction.x * this.speed/FPS;
-        this.y += this.direction.y * this.speed/FPS;
-    }
-};
+        move: function(){
+        if( isCollided(enemyPath[this.pathDes].x, enemyPath[this.pathDes].y, this.x, this.y, this.speed/FPS, this.speed/FPS) ){
+
+            // 首先，移動到下一個路徑點
+            this.x = enemyPath[this.pathDes].x;
+            this.y = enemyPath[this.pathDes].y;
+
+            // 指定下一個路徑點
+            this.pathDes++;
+
+            // 取得前往下一個路徑點的單位向量
+            var unitVector = getUnitVector( this.x, this.y, enemyPath[this.pathDes].x, enemyPath[this.pathDes].y );
+            this.direction.x = unitVector.x;
+            this.direction.y = unitVector.y;
+
+        } else {
+            // this.x += this.direction.x * this.speed/FPS;
+            this.x = this.x + this.direction.x * this.speed/FPS;
+            // this.y += this.direction.y * this.speed/FPS;
+            this.y = this.y + this.direction.y * this.speed/FPS;
+        }
+    };
 
 var enemyPath = [
     {x:96, y:64},
